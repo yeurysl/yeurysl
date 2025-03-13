ffbda9b8599c9e4ec16e0856049060c3d3c5dfbf
yeurysl <yeurysl17@gmail.com>
2025-03-12 20:18:07 -0400

    Resolved merge conflicts

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the port from the environment variable (Heroku assigns this automatically)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local testing
+    app.run(host="0.0.0.0", port=port)
